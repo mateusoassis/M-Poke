e5c61391c45ee62c0bde726bcdb87a7e43b92c6e
Mateus Assis <mateusoassis@gmail.com>
2025-04-29 15:20:37 -0300

    importa e organiza nome do pokemon, golpes, PP e tipo com sucesso

--- Assets/Scripts/PokeGetter.cs
+++ Assets/Scripts/PokeGetter.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Net.Cache;
 using System;
 using System.Security.AccessControl;
@@ -10,6 +11,7 @@ public class PokeGetter : MonoBehaviour
 {
     [SerializeField] private string url = "https://pokeapi.co/api/v2/pokemon/ditto";
     public List<string> listaPokemonComMoves = new List<string>();
+    public pokemonInfoCustom pokeInfoCustom;
 
     [System.Serializable]
     public class PokeData
@@ -36,20 +38,63 @@ public class PokeGetter : MonoBehaviour
     public class Move
     {
         public string name;
+        public string url;
     }
 
     [System.Serializable]
-    public class pokeShow
+    public class MoveStats
     {
-        public string pokeName;
-//        public Array
+        public int pp;
+        public MoveType type;
     }
 
+    [System.Serializable]
+    public class MoveType
+    {
+        public string name;
+    }
+
+    [System.Serializable]
+    public class pokemonInfoCustom
+    {
+        public string pokeName;
+        public List<MoveData> pokeMoves;
+    }
+
+    [System.Serializable]
+    public class MoveData
+    {
+        public string name;
+        public string type;
+        public int pp;
+    }
+
+    void Awake()
+    {
+        pokeInfoCustom.pokeMoves = new List<MoveData>{null, null, null, null};
+    }
     void Start()
     {
         StartCoroutine(GetPokeData(url));
     }
-    
+    IEnumerator GetMoveDataAndSave(MoveData getMoveData, string url, List<MoveData> saveMoveData, int index)
+    {
+        UnityWebRequest request = UnityWebRequest.Get(url);
+        yield return request.SendWebRequest();
+
+        if(request.result == UnityWebRequest.Result.Success)
+        {
+            // saveMoveData.Add(getMoveData);
+            MoveStats moveStats = JsonUtility.FromJson<MoveStats>(request.downloadHandler.text);
+            getMoveData.pp = moveStats.pp;
+            getMoveData.type = moveStats.type.name;
+            saveMoveData[index] = getMoveData;
+        }
+        else
+        {
+            Debug.Log("Erro: " + request.error);
+        }
+    }
     IEnumerator GetPokeData(string url)
     {
         UnityWebRequest request = UnityWebRequest.Get(url);
@@ -59,13 +104,18 @@ public class PokeGetter : MonoBehaviour
         {
             //Debug.Log(request.downloadHandler.text);
             PokeData pokemonInfo = JsonUtility.FromJson<PokeData>(request.downloadHandler.text);
-            Debug.Log(pokemonInfo.name);
-            listaPokemonComMoves.Add(pokemonInfo.name);
+            // Debug.Log(pokemonInfo.name);
+            pokeInfoCustom.pokeName = pokemonInfo.name;
+            // pokeInfoCustom.pokeMoves.Add(pokemonInfo.moves[i].move.name);
             if(pokemonInfo.moves.Length > 4)
             {
                 for(int i = 0; i < 4; i++)
                 {
-                    listaPokemonComMoves.Add(pokemonInfo.moves[i].move.name);
+                    MoveData moveData = new MoveData();
+                    moveData.name = pokemonInfo.moves[i].move.name;
+                    StartCoroutine(GetMoveDataAndSave(moveData, pokemonInfo.moves[i].move.url, pokeInfoCustom.pokeMoves, i));
+                    // pokeInfoCustom.pokeMoves[i].name = pokemonInfo.moves[i].move.name;
+                    // pokeInfoCustom.pokeMoves.Add(moveData);
                 }
             }
             else if(pokemonInfo.moves.Length <= 3)
@@ -74,15 +124,46 @@ public class PokeGetter : MonoBehaviour
                 {
                     if (i+1 <= pokemonInfo.moves.Length)
                     {
-                        listaPokemonComMoves.Add(pokemonInfo.moves[i].move.name);
+                        MoveData moveData = new MoveData();
+                        moveData.name = pokemonInfo.moves[i].move.name;
+                        StartCoroutine(GetMoveDataAndSave(moveData,pokemonInfo.moves[i].move.url,pokeInfoCustom.pokeMoves, i));
+                        // pokeInfoCustom.pokeMoves[i].name = pokemonInfo.moves[i].move.name;
+                        // pokeInfoCustom.pokeMoves.Add(moveData);
                     }
                     else
                     {
-                        listaPokemonComMoves.Add("--");
+                        MoveData moveData = new MoveData();
+                        moveData.name = "--";
+                        // pokeInfoCustom.pokeMoves[i].name = "--";
+                        pokeInfoCustom.pokeMoves[i] = moveData;
                     }
                 }
-                
             }
+                
+            // }
+            // listaPokemonComMoves.Add(pokemonInfo.name);
+            // if(pokemonInfo.moves.Length > 4)
+            // {
+            //     for(int i = 0; i < 4; i++)
+            //     {
+            //         listaPokemonComMoves.Add(pokemonInfo.moves[i].move.name);
+            //     }
+            // }
+            // else if(pokemonInfo.moves.Length <= 3)
+            // {
+            //     for(int i = 0; i < 4; i++)
+            //     {
+            //         if (i+1 <= pokemonInfo.moves.Length)
+            //         {
+            //             listaPokemonComMoves.Add(pokemonInfo.moves[i].move.name);
+            //         }
+            //         else
+            //         {
+            //             listaPokemonComMoves.Add("--");
+            //         }
+            //     }
+                
+            // }
         }
         else
         {
