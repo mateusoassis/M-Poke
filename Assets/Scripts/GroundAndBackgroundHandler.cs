fd8fed61f6e82b26b9048871d2dbfe96881278f1
Mateus Assis <mateusoassis@gmail.com>
2025-05-01 09:08:16 -0300

    Randomização de player/enemy grounds e background + fix dos sprites de forest/grass

---
+++ Assets/Scripts/GroundAndBackgroundHandler.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GroundAndBackgroundHandler : MonoBehaviour
+{
+    [System.Serializable]
+    public class CustomGround
+    {
+        public Sprite background;
+        public Sprite playerGround;
+        public Sprite enemyGround;
+    }
+
+    [SerializeField] private Image playerGroundTarget;
+    [SerializeField] private Image enemyGroundTarget;
+    [SerializeField] private Image backgroundTarget;
+    [SerializeField] private CustomGround[] grounds;
+
+    void Awake()
+    {
+        SwapAllGrounds();
+    }
+
+    private void SwapAllGrounds()
+    {
+        int i = Random.Range(0, grounds.Length);
+        playerGroundTarget.sprite = grounds[i].playerGround;
+        enemyGroundTarget.sprite = grounds[i].enemyGround;
+        backgroundTarget.sprite = grounds[i].background;
+    }
+}
