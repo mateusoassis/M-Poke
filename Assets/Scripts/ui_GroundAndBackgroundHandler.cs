edc29a9f6bcd1acd3945b24d0914c5edd8910d66
Mateus Assis <mateusoassis@gmail.com>
2025-05-01 10:09:52 -0300

    Padronização do prefixo ui_ em scripts de interface (GroundAndBackgroundHandler para ui_GroundAndBackgroundHandler)

--- Assets/Scripts/ui_GroundAndBackgroundHandler.cs
+++ Assets/Scripts/ui_GroundAndBackgroundHandler.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GroundAndBackgroundHandler : MonoBehaviour
+public class ui_GroundAndBackgroundHandler : MonoBehaviour
 {
     [System.Serializable]
     public class CustomGround
